6af5d30d834a8a17cc0dff73ab53f8470f441bee
SanderRonde <awsdfgvhbjn@gmail.com>
2017-04-25 00:43:47 +0200

    fixed caching targets and offline functioning

--- app/client/src/js/sw/serviceworker.ts
+++ app/client/src/js/sw/serviceworker.ts
@@ -8,9 +8,9 @@ toolbox.options.cache.name = 'IedereenLagInEenDeukCache';
 toolbox.options.cache.maxEntries = 500;
 toolbox.options.cache.maxAgeSeconds = 60 * 60 * 24 * (365.25 / 12);
 
-const toCache = ['main.css', 'main.js', '/404']
+const toCache = ['/js/main.js', '/css/main.css', '/css/main_offline.css']
 	.concat('/', '/404', '/offline')
-	.concat('/fonts/Roboto-Regular.ttf')
+	.concat('/fonts/Roboto-Regular.ttf', '/resources/vid.mp4')
 	.concat('/about/manifest.json', '/about/images/48.png',
 		'/about/images/72.png', '/about/images/96.png',
 		'/about/images/144.png', '/about/images/168.png',
@@ -32,12 +32,27 @@ toolbox.precache(toCache);
 self.addEventListener('install', async (event) => {
 	event.waitUntil(new Promise(async (resolve) => {
 		
-		toolbox.router.any('/', toolbox.fastest);
+		toolbox.router.any('/', (req) => {
+			return new Promise((resolve) => {
+				let path: string;
+				if (navigator.onLine) {
+					path = '/';
+				} else {
+					path = '/offline'
+				}
+				caches.open(toolbox.options.cache.name).then((cache) => {
+					cache.match(path).then((res) => {
+						resolve(res);
+					});
+				})
+			});
+		});
 		toolbox.router.any('/404', toolbox.fastest);
 		toolbox.router.any('/fonts/*', toolbox.cacheOnly);
 		toolbox.router.any('/css/(.*)_offline.css', toolbox.cacheOnly);
 		toolbox.router.any('/css/(.*).css', toolbox.fastest);
 		toolbox.router.any('/js/*', toolbox.fastest);
+		toolbox.router.any('/resources/*', toolbox.fastest);
 		toolbox.router.any('/about/*', toolbox.fastest);
 
 		toolbox.router.default = (req, values, options) => {
