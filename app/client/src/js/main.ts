29854944b2451ceca39419b09304eff227655ef5
SanderRonde <awsdfgvhbjn@gmail.com>
2017-05-08 20:31:47 +0200

    added toast code/css

--- app/client/src/js/main.ts
+++ app/client/src/js/main.ts
@@ -1,5 +1,82 @@
 /// <reference path="../../../../typings/client.d.ts" />
 
+const toasts: Array<Toast> = [];
+export class Toast {
+	element: HTMLElement;
+
+	constructor(public text: string, public duration: number, buttons: Array<{
+		text: string;
+		callback(): void;
+	}> = []) {
+		this.element = this.createElement(buttons);
+		this.showWhenReady();
+		toasts.push(this);
+	}
+
+	private showWhenReady() {
+		if (toasts.length === 0) {
+			window.setTimeout(() => {
+				this.show();
+			}, 10);
+		}
+	}
+
+	hide() {
+		this.element.classList.add('hidden');
+		window.setTimeout(() => {
+			this.destroy();
+		}, 1000);
+	}
+
+	private destroy() {
+		this.element.remove();
+
+		toasts.splice(toasts.indexOf(this), 1);
+		if (toasts[0]) {
+			toasts[0].show();
+		}
+	}
+
+	show() {
+		this.element.classList.remove('hidden');
+		window.setTimeout(() => {
+			this.hide();
+		}, this.duration);
+	}
+
+	private createButton(text: string, onclick: () => void): HTMLElement {
+		const elButton = document.createElement('span');
+		elButton.classList.add('toastDismiss');
+		elButton.classList.add('fancyOnClick');
+		elButton.addEventListener('click', onclick);
+		elButton.innerText = text;
+		return elButton;
+	}
+
+	private createElement(buttons: Array<{
+		text: string;
+		callback(): void;
+	}>): HTMLElement {
+		const el = document.createElement('div');
+		el.classList.add('toast','hidden');
+
+		const elText = document.createElement('div');
+		elText.classList.add('toastText');
+		elText.innerText = this.text;
+
+		el.appendChild(elText);
+
+		buttons.forEach((button) => {
+			el.appendChild(this.createButton(button.text, button.callback));
+		});
+		el.appendChild(this.createButton('WEGDOEN', this.hide.bind(this)));
+
+		document.body.appendChild(el);
+
+		return el;
+	}
+}
+
 if (navigator.onLine && location.protocol === 'https:') {
 	navigator.serviceWorker.register('/serviceworker.js');
 }
